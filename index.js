1f57172fff88f6c8a13bcbd76ea4bfc07472eb42
Arthur Oliveira <arthurviniciusdo@gmail.com>
2025-01-04 14:46:56 -0300

    Revert "Update 1.2"

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
-let buttonMenu = document.querySelector('.menu-button');
+function toggleMenu() {
+    var opcoes = document.getElementById('menu-options');
 
-buttonMenu.addEventListener("click", () => {
-
-    let optionsMenu = document.querySelector('.menu-options');
-
-    let optionsMenuDisplay = window.getComputedStyle(optionsMenu).display;
-
-    if(optionsMenuDisplay === 'none'){
-
-        optionsMenu.style.display = 'block'
+    if(opcoes.style.display == 'none'){
+        opcoes.style.display = 'flex';
     }
-
     else{
-        optionsMenu.style.display = 'none'
+        opcoes.style.display = 'none';
     }
-})
-
-let buttons = document.querySelectorAll('.button-conteudo');
-
-buttons.forEach((button, index) => {
-    
-    button.addEventListener("click", () => {
-        
-        let arrow = button.querySelector('.icone-serviços');
-        let conteudo = document.querySelector(`#conteudo${index + 1}`);
-
-        let conteudoDisplay = window.getComputedStyle(conteudo).display;
-
-        if (conteudoDisplay === 'none' || conteudoDisplay === '') {
-
-            conteudo.style.display = 'flex'; 
-            arrow.style.transform = 'rotate(90deg)'; 
-        } 
-        
-        else {
-            
-            conteudo.style.display = 'none'; 
-            arrow.style.transform = 'rotate(0deg)'; 
-        }
-    });
-});
+}
